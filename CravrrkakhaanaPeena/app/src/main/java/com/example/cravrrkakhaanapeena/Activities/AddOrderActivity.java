7fb107a7d67a6f24770cd06d2c6bb06a6077c711
Keshri13 <keshri1316@gmail.com>
2022-12-21 00:29:13 +0530

    Started UI Implementation for Employee Side

---
+++ CravrrkakhaanaPeena/app/src/main/java/com/example/cravrrkakhaanapeena/Activities/AddOrderActivity.java
@@ -0,0 +1,16 @@
+package com.example.cravrrkakhaanapeena.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cravrrkakhaanapeena.R;
+
+public class AddOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_order);
+    }
+}
