38305d8669f8b76a3f97f8e9650e2415bb4ae4b1
Keshri13 <keshri1316@gmail.com>
2022-12-21 23:08:08 +0530

    Added Splash Screen and AddOrder Screen

--- CravrrkakhaanaPeena/app/src/main/java/com/example/cravrrkakhaanapeena/Activities/AddOrderActivity.java
+++ CravrrkakhaanaPeena/app/src/main/java/com/example/cravrrkakhaanapeena/Activities/AddOrderActivity.java
@@ -3,14 +3,65 @@ package com.example.cravrrkakhaanapeena.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.cravrrkakhaanapeena.R;
 
-public class AddOrderActivity extends AppCompatActivity {
+public class AddOrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner EmployeeNameSpinner;
+    EditText OrderItem,TotalAmount;
+
+    // create array of Strings
+    // and store name of courses
+    String[] Employees = { "Arpan","Vaivhav","Ishika","Nidhi","Shiwangi" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_order);
+
+        EmployeeNameSpinner = findViewById(R.id.employeeNameSpinner);
+        EmployeeNameSpinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+
+        // Create the instance of ArrayAdapter
+        // having the list of courses
+        ArrayAdapter ad
+                = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                Employees);
+
+        // set simple layout resource file
+        // for each item of spinner
+        ad.setDropDownViewResource(
+                android.R.layout
+                        .simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        EmployeeNameSpinner.setAdapter(ad);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        // make toastof name of course
+        // which is selected in spinner
+        Toast.makeText(getApplicationContext(),
+                        Employees[i],
+                        Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
--- CravrrkakhaanaPeena/app/src/main/java/com/example/cravrrkakhaanapeena/Activities/MainActivity.java
+++ CravrrkakhaanaPeena/app/src/main/java/com/example/cravrrkakhaanapeena/Activities/MainActivity.java
@@ -1,8 +1,12 @@
-package com.example.cravrrkakhaanapeena;
+package com.example.cravrrkakhaanapeena.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.cravrrkakhaanapeena.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after delay
+                finish();
+                startActivity(new Intent(MainActivity.this,AddOrderActivity.class));
+            }
+        }, 3000);
     }
+
 }
